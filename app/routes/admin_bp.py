2ab27d542ed1fdb7386f80d966a641edd4efcc5e
Ariaspect <keeminjoon@kaist.ac.kr>
2024-12-22 10:08:12 +0900

    type(admin): change menu to product

--- app/routes/admin_bp.py
+++ app/routes/admin_bp.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, jsonify
+from flask import Blueprint, render_template, url_for, redirect, request
 from app.models import db, Product
 
 admin_bp = Blueprint('admin', __name__, url_prefix='/admin')
@@ -10,47 +10,25 @@ def admin_index():
     return render_template('admin/index.html', title="Admin Dashboard")
 
 
-@admin_bp.route('/menu')
-def admin_menu():
-    menu_items = [
-        {"id": 1, "name": "Burger", "price": 8.99},
-        {"id": 2, "name": "Pizza", "price": 12.99},
-    ]
-    return render_template('admin/menu.html', title="Admin Menu", menu_items=menu_items)
+@admin_bp.route('/product')
+def admin_product():
+    products = Product.query.all()
+    return render_template('admin/product.html', title="Admin Menu", product_list=products)
 
 
-# @admin_bp.route('/product')
-# def admin_menu():
-#     menu_items = [
-#         {"id": 1, "name": "Burger", "price": 8.99},
-#         {"id": 2, "name": "Pizza", "price": 12.99},
-#     ]
-#     return render_template('menu.html', title="Admin Menu", menu_items=menu_items)
-
-
-# @admin_bp.route('/stat')
-# def admin_menu():
-#     menu_items = [
-#         {"id": 1, "name": "Burger", "price": 8.99},
-#         {"id": 2, "name": "Pizza", "price": 12.99},
-#     ]
-#     return render_template('menu.html', title="Admin Menu", menu_items=menu_items)
-
-
-@admin_bp.route('/add_menu', methods=['POST'])
-def add_menu():
-    # Parse the JSON data sent by the fetch request
-    data = request.get_json()
-
-    # Extract 'name' and 'price' from the parsed JSON
-    product_name = data.get('name')
-    product_price = data.get('price')
-    product_available = data.get('available', True)
+@admin_bp.route('/add_product', methods=['POST'])
+def add_product():
+    product_name = request.form.get('name')
+    product_price = request.form.get('price', type=int)
+    product_available = 'available' in request.form
 
     new_product = Product(
         name=product_name, price=product_price, available=product_available)
     db.session.add(new_product)
     db.session.commit()
+
+    return redirect(url_for('admin.admin_product'))
+
 
     # Return a JSON response
     return jsonify({'status': 'success', 'name': product_name, 'price': product_price})
