f220f14015331a3a39f22272796d0726075e769e
Jamie Son <jamieson@Jamies-MacBook-Air.local>
2024-12-31 23:07:55 +0900

    add_product.html에 option 추가,admin_bp.py에 grouped_option 만들어서 정렬 후에 보냄

--- config.py
+++
@@ -1,6 +0,0 @@
-class Config:
-    SQLALCHEMY_DATABASE_URI = "sqlite:////Users/jamieson/operation-ricecake/database.db"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-    SECRET_KEY = 'your_secret_key_here'
-    DEBUG = True
