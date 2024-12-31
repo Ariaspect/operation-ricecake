8c3c793363b1d36cb99781f46cca3420ef08b56b
Jamie Son <jamieson@Jamies-MacBook-Air.local>
2024-12-31 15:56:52 +0900

    hi

--- config.py
+++ config.py
@@ -1,7 +1,5 @@
-
-
 class Config:
-    SQLALCHEMY_DATABASE_URI = "sqlite:////home/ariaspect/oprc/database.db"
+    SQLALCHEMY_DATABASE_URI = "sqlite:////Users/jamieson/operation-ricecake/database.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     SECRET_KEY = 'your_secret_key_here'
