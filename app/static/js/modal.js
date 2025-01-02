35feff5b7cf058389de379d2ed90ccc185325c93
Ariaspect <keeminjoon@kaist.ac.kr>
2025-01-02 20:06:11 +0900

    fix(admin): init select fields in modal if exist

--- app/static/js/modal.js
+++ app/static/js/modal.js
@@ -2,6 +2,9 @@ async function open_modal(url) {
   const response = await fetch(url);
   const modalHtml = await response.text();
   document.querySelector("#modal_container").innerHTML = modalHtml;
+
   const instance = M.Modal.init(document.querySelector(".modal"));
+  M.FormSelect.init(document.querySelectorAll("#modal_container select"));
+
   instance.open();
 }
