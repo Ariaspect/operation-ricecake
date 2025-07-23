c2d9f963bf314b95fb99127dd2ac270594bfaf51
Ariaspect <keeminjoon@kaist.ac.kr>
2025-07-23 16:04:53 +0900

    feat(db): added order related models

--- app/models.py
+++ app/models.py
@@ -47,3 +47,40 @@ class ProductOption(db.Model):
         db.Integer, db.ForeignKey("product.product_id"), nullable=False
     )
     option_id = db.Column(db.Integer, db.ForeignKey("option.option_id"), nullable=False)
+
+
+# Order Table
+class Order(db.Model):
+    __tablename__ = "order"
+    order_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    date = db.Column(db.DateTime, nullable=False)
+    address = db.Column(db.String(255), nullable=False)
+    payment = db.Column(db.String(255), nullable=False)
+    memo = db.Column(db.Text)
+
+    # Relationships
+    details = db.relationship("OrderDetail", backref="order", lazy=True)
+
+
+# OrderDetail Table
+class OrderDetail(db.Model):
+    __tablename__ = "order_detail"
+    order_detail_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    order_id = db.Column(db.Integer, db.ForeignKey("order.order_id"), nullable=False)
+    product_id = db.Column(
+        db.Integer, db.ForeignKey("product.product_id"), nullable=False
+    )
+    quantity = db.Column(db.Integer, nullable=False)
+
+    # Relationships
+    options = db.relationship("OrderDetailOption", backref="order_detail", lazy=True)
+
+
+# OrderDetailOption Table
+class OrderDetailOption(db.Model):
+    __tablename__ = "order_detail_option"
+    order_detail_option_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    order_detail_id = db.Column(
+        db.Integer, db.ForeignKey("order_detail.order_detail_id"), nullable=False
+    )
+    option_id = db.Column(db.Integer, db.ForeignKey("option.option_id"), nullable=False)
